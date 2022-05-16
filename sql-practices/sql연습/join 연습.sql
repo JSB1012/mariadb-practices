f348db00e086d963a067377f55eebf42d45f7113
JSB1012 <sbj15102@naver.com>
2022-05-16 11:28:27 +0900

    join #finish

--- sql-practices/sql연습/join 연습.sql
+++ sql-practices/sql연습/join 연습.sql
@@ -1,12 +1,13 @@
 -- inner join
 
--- 예제1: 현재 근무하고 직원의 이름과 직책을 출력 하세요. 
-select a.emp_no, a.first_name, b.title
-  from employees a, titles b
- where a.emp_no = b.emp_no        -- join 조건(n-1)
-   and b.to_date = '9999-01-01';  -- row 선택 조건
-
--- 예제2: 현재 직원의 이름과 직책을 출력하되 여성 엔지니어(engineer)만 출력하게요.
+SELECT 
+    a.emp_no, a.first_name, b.title
+FROM
+    employees a,
+    titles b
+WHERE
+    a.emp_no = b.emp_no
+        AND b.to_date = '9999-01-01'엔지니어(engineer)만 출력하게요.
 select a.emp_no, a.first_name, a.gender, b.title
   from employees a, titles b
  where a.emp_no = b.emp_no        -- join 조건(n-1)
@@ -37,11 +38,77 @@ select count(*)
    and b.to_date = '9999-01-01';
 
 -- 3) join ~ on
-  select b.title, avg(a.salary)
-    from salaries a 
-    join titles b on a.emp_no = b.emp_no
-   where a.to_date = '9999-01-01'
-     and b.to_date = '9999-01-01'
+SELECT 
+    b.title, AVG(a.salary)
+FROM
+    salaries a
+        JOIN
+    titles b ON a.emp_no = b.emp_no
+WHERE
+    a.to_date = '9999-01-01'
+    and b.to_date = '9999-01-01'
 group by b.title
 order by avg(a.salary) desc;  
-   
+
+-- 실습문제 1: 현재 회사 상황을 반영한 직원별 근무부서를 사번, 직원 전체이름 근무부서 형태로 출력해보세요.
+SELECT 
+    a.emp_no, a.first_name, b.dept_name
+FROM
+    employees a,
+    departments b,
+    dept_emp c
+WHERE
+    a.emp_no = c.emp_no
+        AND b.dept_no = c.dept_no
+        AND c.to_date = '9999-01-01';
+    
+-- 실습문제 2: 현재 회사에서 지급되고 잇는 급여체계를 반영한 결과를 출력하세요. 사번, 전체이름, 연봉 이런 형태로
+select a.emp_no, a.first_name, b.salary
+from employees a, salaries b
+where  a.emp_no = b.emp_no
+and b.to_date = '9999-01-01'
+order by b.salary desc;
+
+-- 실습문제 3: 현재 직책별로 평균 연봉과 인원수를 구하되 직책별로 인원이 100명 이상인 직책만 출력하세요.
+SELECT 
+    a.title, AVG(b.salary), COUNT(*)
+FROM
+    titles a,
+    salaries b
+WHERE
+    a.emp_no = b.emp_no
+        AND a.to_date = '9999-01-01'
+        AND b.to_date = '9999-01-01'
+GROUP BY a.title
+HAVING COUNT(*) >= '100'
+ORDER BY AVG(b.salary) DESC;
+
+-- 실습문제 4:현재 부서별로 현재 직책이 Engineer인 직원들의 대해서만 평균 급여를 구하세요.
+SELECT 
+    AVG(c.salary), a.dept_name, b.title
+FROM
+    departments a,
+    titles b,
+    salaries c,
+    dept_emp d
+WHERE
+    a.dept_no = d.dept_no
+        AND d.emp_no = c.emp_no
+        AND c.emp_no = b.emp_no
+        AND d.to_date = '9999-01-01'
+        AND b.to_date = '9999-01-01'
+        AND b.title = 'Engineer'
+GROUP BY a.dept_name;
+
+
+select d.dept_name, avg(salary)
+from dept_emp a, salaries b, titles c, departments d
+where a.emp_no = b.emp_no
+and b.emp_no = c.emp_no
+and d.dept_no = a.dept_no
+and a.to_date = '9999-01-01'
+and b.to_date = '9999-01-01'
+and c.to_date = '9999-01-01'
+and c.title = 'Engineer'
+group by d.dept_name;
+
