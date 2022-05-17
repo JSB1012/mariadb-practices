7cfe92329428ee47b7c2c38e577d47d408189804
JSB1012 <sbj15102@naver.com>
2022-05-17 15:22:14 +0900

    driver 작동 원리

--- jdbc-practices/src/main/java/test/ConnectionTest.java
+++ jdbc-practices/src/main/java/test/ConnectionTest.java
@@ -5,16 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionTest {
-	
-	
 	public static void main(String[] args) {
+		Connection connection = null;
+		
 		try {
 			//1. JDBC Driver 로딩 (JDBC Class 로딩: class loader)
 			Class.forName("org.mariadb.jdbc.Driver");
 			
 			//2. 연결하기
 			String url = "jdbc:mysql://192.168.10.55:3306/webdb?charset=utf8";
-			Connection connection = DriverManager.getConnection(url, "webdb", "webdb");
+			connection = DriverManager.getConnection(url, "webdb", "webdb");
 			
 			System.out.println("connected!");
 			
@@ -22,11 +22,14 @@ public class ConnectionTest {
 			System.out.println("드라이버 로딩 실패:" + e);
 		} catch (SQLException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
+		} finally {
+			try {
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		
-		
-
-
 	}
-
 }
